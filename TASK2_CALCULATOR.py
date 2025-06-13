af4893c5a0a561d55067bf8e54c2747d61655d87
JIJENDRA M <m.jijendra@gmail.com>
2025-06-13 22:48:20 +0530

    Update TASK2_CALCULATOR.py

--- TASK2_CALCULATOR.py
+++ TASK2_CALCULATOR.py
@@ -1,21 +1,29 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-print("Choose an operation: +  -  *  /")
-operation = input("Enter your choice: ")
-if operation == "+":
-    result = num1 + num2
-    print("The result is:", result)
-elif operation == "-":
-    result = num1 - num2
-    print("The result is:", result)
-elif operation == "*":
-    result = num1 * num2
-    print("The result is:", result)
-elif operation == "/":
-    if num2 != 0:
-        result = num1 / num2
+while True:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    print("Choose an operation: +  -  *  /")
+    operation = input("Enter your choice: ")
+
+    if operation == "+":
+        result = num1 + num2
         print("The result is:", result)
+    elif operation == "-":
+        result = num1 - num2
+        print("The result is:", result)
+    elif operation == "*":
+        result = num1 * num2
+        print("The result is:", result)
+    elif operation == "/":
+        if num2 != 0:
+            result = num1 / num2
+            print("The result is:", result)
+        else:
+            print("Oops! Division by zero is not allowed.")
     else:
-        print("Oops! Division by zero is not allowed.")
-else:
-    print("That operation is not recognized.")
+        print("That operation is not recognized.")
+
+    choice = input("Do you want to perform another calculation? (yes/no): ").lower()
+    if choice != "yes":
+        print("Thank you for using the calculator")
+        break
